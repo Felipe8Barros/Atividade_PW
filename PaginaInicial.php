60be5a905c6b3de3f673f46b989c4e6cd435296d
Felipe Barros <100451395+Felipe8Barros@users.noreply.github.com>
2022-06-28 13:49:51 -0300

    Update PaginaInicial.php

--- PaginaInicial.php
+++ PaginaInicial.php
@@ -9,54 +9,60 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GAMES</title>
+    <title> GAMES </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <style>
   body{
-    background-color: #E1F5A9;
+    background-color: #7FFFD4;
   }
+  form [type="submit"] {
+    display: block;
+    margin: auto;
+}
 </style>
 <body>
 
 <div class="dropdown">
   <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">
-    Dropdown button
+    Menu
   </button>
   <ul class="dropdown-menu">
-    <li><a class="dropdown-item active" href="PaginaInicial.php">Pagína Inicial</a></li>
-    <li><a class="dropdown-item" href="Mostrar.php">Mostruário</a></li>
-    <li><a class="dropdown-item "href="Cadastro.php">Cadastro</a>
-    <p></p>
+    <li><center><a class="dropdown-item active" href="PaginaInicial.php">Pagína Inicial</a></center></li>
+    <li><center><a class="dropdown-item" href="Mostrar.php">Mostruário</a></center></li>
+    <li class="dropdown-item"><center>
         <?php if(isset($_SESSION['usuario'])) {?>
-          <a class="nav.link active" href="Cadastro.php"></a>
+          <a class="dropdown-item" href="Cadastro.php">Cadastro</a>
         <?php } else {?>
         <?php if(isset($_SESSION['usuario'])) ?>
-          <a class="nav.item disabled" href="PaginaInicial.php"></a>
+          <a class="dropdown-item disabled" href="PaginaInicial.php">Cadastro</a>
         <?php }?>  
-    </li>
-
-   <?php if(isset($_SESSION['usuario'])){?>
-      <form action="Logout.php" class="d-flex"=>
-        <input type="submit" value="Sair" class="btn btn-outline-info" />
+        </center></li>
+        <p></p>
+        <li><center><h5 class="dropdown-header">Área de Login</h5></center></li>
+    <li><center>    
+    <?php if(isset($_SESSION['usuario'])){?>
+      <form action="Logout.php" class="d-flex">
+        <input type="submit" value="Sair" class="btn btn-outline-info"/>
       </form>
     <?php } else { ?>
       <form action="Login.php" class="d-flex">
         <input type="submit" value="Entrar" class="btn btn-outline-info"/>
-      </form:> 
+    </form>
     <?php } ?>
-
+    </li></center>
   </ul>
 </div> 
 
-  </div>
 </nav>
 <div class="container">
     <div class="row">
         <div class="col">
-            <h1> Mostruário </h1>
-            <p> GAMES </p>
+            <h1> <center> Pagina Inicial </center> </h1>
+            <br></br>
+            <p> Olá seja bem-vindo à página de Games! </p>
+            <p> Execute o arquivo Dados.php para inserir informações no monstruário ou faça login e cadastre um novo game </p>
         </div>
     </div>
 </div>
