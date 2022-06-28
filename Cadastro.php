a92f9c99098c651f1c26a03c4f07ff9a6059d36a
Felipe Barros <100451395+Felipe8Barros@users.noreply.github.com>
2022-06-28 13:44:50 -0300

    Update Cadastro.php

--- Cadastro.php
+++ Cadastro.php
@@ -13,24 +13,37 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
+<style>
+  body{
+    background-color: #7FFFD4;
+  }
+  form [type="submit"] {
+    display: block;
+    margin: auto;
+}
+  div [name="Cadastrar"]{
+    margin: 0;
+  }
+</style>
 <body>
 
 <div class="dropdown">
   <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">
-    Dropdown button
+    Menu
   </button>
   <ul class="dropdown-menu">
-    <li><a class="dropdown-item active" href="PaginaInicial.php">Pagína Inicial</a></li>
-    <li><a class="dropdown-item" href="Mostruário.php">Mostruário</a></li>
-    <li><a  class="dropdown-item "href="Cadastro.php">Cadastro</a>
+    <li><center><a class="dropdown-item active" href="PaginaInicial.php">Pagína Inicial</a></center></li>
+    <li><center><a class="dropdown-item" href="Mostrar.php">Mostruário</a></center></li>
+    <li class="dropdown-item"><center>
         <?php if(isset($_SESSION['usuario'])) {?>
-          <a class="nav.link active" href="Cadastro.php"></a>
-        <?php } else { ?>
+          <a class="dropdown-item" href="Cadastro.php">Cadastro</a>
+        <?php } else {?>
         <?php if(isset($_SESSION['usuario'])) ?>
-          <a class="nav.item disabled" href="PaginaInicial.php"></a>
-        <?php } ?>  
-    </li>
-  
+          <a class="dropdown-item disabled" href="PaginaInicial.php">Cadastro</a>
+        <?php }?>  
+        </center></li>
+        <p></p>
+        <li><center><h5 class="dropdown-header">Área de Login</h5></center></li>
     <?php if(isset($_SESSION['usuario'])){?>
       <form action="Logout.php" class="d-flex">
         <input type="submit" value="Sair" class="btn btn-outline-info"/>
@@ -48,30 +61,35 @@
 <div class="container">
     <div class="row">
         <div class="col">
-            <h1> Mostruário </h1>
+            <h1><center> Cadastrar Jogo </center></h1>
             <form action="Salvar.php" method="POST">
                 <div>
-                    <label for="nome" class="form-label"> Nome: </label>
-                    <input type="text" name="nome" id="nome" plaaceholder="nome" class="form-control" required/>
+                    <h5><label for="nome" class="form-label"> Nome: </label><h5>
+                    <input type="text" name="nome" id="nome" placeholder="Nome do Game" class="form-control" required/>
                 </div>
+                <p></p>
                 <div>
-                    <label for="desenvolvedor" class="form-label"> Desenvolvedor: </label>
-                    <input type="desenvolvedor" name="desenvolvedor" id="desenvolvedor" plaaceholder="desenvolvedor" class="form-control" required/>
+                  <h5><label for="desenvolvedor" class="form-label"> Desenvolvedor: </label><h5>
+                    <input type="desenvolvedor" name="desenvolvedor" id="desenvolvedor" placeholder="Desenvolvedor" class="form-control" required/>
                 </div>
+                <p></p>
                 <div>
-                    <label for="categoria" class="form-label"> Categoria: </label>
-                    <input type="categoria" name="categoria" id="categoria" plaaceholder="categoria" class="form-control" required/>
+                  <h5><label for="categoria" class="form-label"> Categoria: </label><h5>
+                    <input type="categoria" name="categoria" id="categoria" placeholder="Categoria" class="form-control" required/>
                 </div>
+                <p></p>
                 <div>
-                    <label for="loja" class="form-label"> Loja: </label>
-                    <input type="loja" name="loja" id="loja" plaaceholder="loja" class="form-control" required/>
+                  <h5><label for="loja" class="form-label"> Loja: </label><h5>
+                    <input type="loja" name="loja" id="loja" placeholder="Loja de Venda" class="form-control" required/>
                 </div>
+                <p></p>
                 <div>
-                    <label for="console" class="form-label"> Console: </label>
-                    <input type="console" name="console" id="console" plaaceholder="console" class="form-control" required/>
+                  <h5><label for="console" class="form-label"> Console: </label><h5>
+                    <input type="console" name="console" id="console" placeholder="Console" class="form-control" required/>
                 </div>
+                <p></p>
                 <div>
-                    <input type="submit" name="Cadastrar" class="btn btn-primary" />
+                    <input type="submit" name="Cadastrar" value="Cadastrar" class="btn btn-primary" />
                 </div>
              </form>
         </div>
