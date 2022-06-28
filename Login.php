79f185a498730ce558b730e2c426a318a38bc028
Felipe Barros <100451395+Felipe8Barros@users.noreply.github.com>
2022-06-28 13:47:48 -0300

    Update Login.php

--- Login.php
+++ Login.php
@@ -7,26 +7,54 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" text-align: center>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GAMES</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
-<body text-align: center>
+<style>
+  body{
+    background-color: #7FFFD4;
+  }
+  form [type="submit"] {
+    display: block;
+    margin: auto;
+}
+  div [name="login"]{
+    margin: 0;
+  }
+</style>
+<body>
 
 <div class="dropdown">
   <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">
-    Dropdown button
+    Menu
   </button>
   <ul class="dropdown-menu">
-    <li><a class="dropdown-item active" href="PaginaInicial.php">Pagína Inicial</a></li>
-    <li><a class="dropdown-item" href="Mostrar.php">Mostruário</a></li>
-    <li><a class="dropdown-item disabled" href="#">Cadastro</a></li>
-    <p></p>
-    <form action="login.php" class="d-flex">
-      <input type="submit" value="login" class="btn btn-outline-info"/>
+    <li><center><a class="dropdown-item active" href="PaginaInicial.php">Pagína Inicial</a></center></li>
+    <li><center><a class="dropdown-item" href="Mostrar.php">Mostruário</a></center></li>
+    <li class="dropdown-item"><center>
+        <?php if(isset($_SESSION['usuario'])) {?>
+          <a class="dropdown-item" href="Cadastro.php">Cadastro</a>
+        <?php } else {?>
+        <?php if(isset($_SESSION['usuario'])) ?>
+          <a class="dropdown-item disabled" href="PaginaInicial.php">Cadastro</a>
+        <?php }?>  
+        </center></li>
+        <p></p>
+        <li><center><h5 class="dropdown-header">Área de Login</h5></center></li>
+    <?php if(isset($_SESSION['usuario'])){?>
+      <form action="Logout.php" class="d-flex">
+        <input type="submit" value="Sair" class="btn btn-outline-info"/>
+      </form>
+    <?php } else { ?>
+      <form action="Login.php" class="d-flex">
+        <input type="submit" value="Entrar" class="btn btn-outline-info"/>
     </form>
+    <?php } ?>
+  </ul>
+    </li>
   </ul>
 
 </div> 
@@ -36,18 +64,21 @@
 <div class="container">
     <div class="row">
         <div class="col">
-            <h1> Mostruário </h1>
+            <h1> Fazer Login </h1>
             <form action="Validar.php">
+              <p></p>
                 <div>
-                    <label for="nome" class="form-label"> Nome: </label>
-                    <input type="text" name="nome" id="nome" plaaceholder="nome" class="form-control"/>
+                    <h5><label for="nome" class="form-label"> Nome: </label><h5>
+                    <input type="text" name="nome" id="nome" placeholder="Nome" class="form-control"/>
                 </div>
+                <p></p>
                 <div>
-                    <label for="senha" class="form-label"> Senha: </label>
-                    <input type="password" name="senha" id="senha" plaaceholder="senha" class="form-control" />
+                    <h5><label for="senha" class="form-label"> Senha: </label><h5>
+                    <input type="password" name="senha" id="senha" placeholder="Senha" class="form-control" />
                 </div>
+                <p></p>
                 <div>
-                    <input type="submit" name="login" class="btn btn-primary" />
+                    <input type="submit" name="login" value="Login" class="btn btn-primary"/>
                 </div>
              </form>
         </div>
